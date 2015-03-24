db2869ded5f4e09d67577c40516ded2f03eda55c
ppy2wong <admin@ppy2wong.com>
2015-03-23 20:02:52 -0400

    Created scaffolds for ingredient and ingredient type models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :ingredient_types
+
+  resources :ingredients
+
   resources :recipes
 
   # The priority is based upon order of creation: first created -> highest priority.
