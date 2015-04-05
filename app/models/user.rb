2e5006463bc89516f95670fb7985c2a4fb5e80b6
ppy2wong <admin@ppy2wong.com>
2015-04-05 15:32:47 -0400

    Added Devise user authentication, refactored changes to models into partials, and enabled / disabled edit functionality for admin / regular users (respectively) as appropriate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,24 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
   has_one :dietary_preference
+
+  # Check if a user is an admin user
+  def admin_user?
+  	return self.user_category == "admin"
+  end
+
+  # Check if a user is a vendor
+  def vendor_user?
+  	return self.user_category == "vendor"
+  end
+
+  # Check if a user has a profile
+  def has_profile?
+    return self.profile.present?
+  end
+
+  # Check if a user has dietary preferences set
+  def has_dietary_preferences?
+    return self.dietary_preference.present?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404183006) do
+ActiveRecord::Schema.define(version: 20150405164801) do
 
   create_table "dietary_preferences", force: :cascade do |t|
     t.integer  "user_id"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20150404183006) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_category"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
