5efd6578ad2c501cb28afbd73767f4a850ff54ad
jennaleeb <jennaleeblumenthal@gmail.com>
2015-04-12 18:59:08 -0400

    Weird env & id-key change

--- config/initializers/yummly.rb
+++ config/initializers/yummly.rb
@@ -1,5 +1,5 @@
 Yummly.configure do |config|
   config.app_id = "128e497b"
-  config.app_key = "1816292b78dc608e22491c1e1799fcf9"
+  config.app_key = "f7f6fc3301b4e15d112af7218480c305"
   config.use_ssl = true # Default is false
 end
