68b79907fbb8fe7e19eba9011cd2114b54e7829b
ppy2wong <admin@ppy2wong.com>
2015-05-02 19:07:30 -0400

    Added mechanism to update location in Shop search

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,10 +1,10 @@
 class Diet < ActiveRecord::Base
 	# A diet consists of many ingredient types
-	has_many :diet_ingredient_types
+	has_many :diet_ingredient_types, dependent: :destroy
 	has_many :ingredient_types, through: :diet_ingredient_types
 
 	# A diet can be followed by many users (via the users' dietary preferences)
-	has_many :dietary_preferences
+	has_many :dietary_preferences, dependent: :destroy
 	has_many :users, through: :dietary_preferences
 
 	# A diet must have a name
