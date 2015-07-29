ef016197d6fa06e296d20ba1d0bba6339fac375f
jennaleeb <jennaleeblumenthal@gmail.com>
2015-07-29 11:43:55 -0400

    moved logic of finding name and thumbail from user fav recipes to model method

--- app/models/user_favourite_recipe.rb
+++ app/models/user_favourite_recipe.rb
@@ -9,4 +9,12 @@ class UserFavouriteRecipe < ActiveRecord::Base
   def self.has_user_favourites_for_recipe?(user_id, yummly_id)
     return self.find_by(user_id: user_id, yummly_id: yummly_id)
   end
+
+  def find_recipe_name
+  	return Recipe.find(self.recipe_id).name
+  end
+
+  def find_recipe_thumbnail
+  	return Recipe.find(self.recipe_id).thumbnail
+  end
 end
